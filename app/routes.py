52ad235bb75bc4fa7ffc119700d298d68fde9a36
Brian Wilson <brianwilson1001@hotmail.com>
2018-11-04 14:03:57 +0000

    Add tolearn process

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,12 @@ from flask import request
 from app import app
 
 
-@app.route('/update_practiced')
-def update_practiced():
+@app.route('/get_songs')
+def get_songs():
   songs = get_songlist()
-  return json.dumps(sorted(songs, key=lambda x: (x['lastPracticed'], x['name'])))
+  tolearn = get_tolearn()
+  data = {'songs': songs, 'tolearn': tolearn}
+  return json.dumps(data)
 
 
 @app.route('/add_song', methods=['POST'])
@@ -25,6 +27,33 @@ def add_song():
   return json.dumps(sorted(songs, key=lambda x: (x['lastPracticed'], x['name'])))
 
 
+@app.route('/set_practiced', methods=['POST'])
+def set_practiced():
+  songs = get_songlist()
+  for song in songs:
+    if song['name'] == request.json['name']:
+      song['lastPracticed'] = time.mktime(date.today().timetuple())
+      break
+  write_songlist(songs)
+  return json.dumps(sorted(songs, key=lambda x: (x['lastPracticed'], x['name'])))
+
+
+@app.route('/move_to_current', methods=['POST'])
+def move_to_current():
+  songs = get_songlist()
+  tolearn = get_tolearn()
+  tolearn.remove(request.json['name'])
+  new_current = {
+    'name': request.json['name'],
+    'lastPracticed': time.mktime(date.today().timetuple())
+  }
+  songs.append(new_current)
+  write_songlist(songs)
+  write_tolearn(tolearn)
+  data = {'songs': songs, 'tolearn': tolearn}
+  return json.dumps(data)
+
+
 def get_songlist():
   with open(os.path.join(os.path.dirname(__file__), 'songlist.json'), 'r') as f:
     return json.load(f)
@@ -33,3 +62,13 @@ def get_songlist():
 def write_songlist(songs):
   with open(os.path.join(os.path.dirname(__file__), 'songlist.json'), 'w') as f:
     json.dump(songs, f)
+
+
+def get_tolearn():
+  with open(os.path.join(os.path.dirname(__file__), 'tolearn.json'), 'r') as f:
+    return json.load(f)
+
+
+def write_tolearn(tolearn):
+  with open(os.path.join(os.path.dirname(__file__), 'tolearn.json'), 'w') as f:
+    json.dump(tolearn, f)
